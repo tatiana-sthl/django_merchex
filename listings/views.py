ba144af2bc98eb83afebfbdacb7bfb88920f2fbc
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-03-21 20:07:32 +0100

    add templates and static

--- listings/views.py
+++ listings/views.py
@@ -5,30 +5,14 @@ from listings.models import Band, Title
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-    <h1>Hello Django !</h1>
-    <p>Mes groupes préférés sont :</p>
-    <ul>
-        <li>{bands[0].name}</li>
-        <li>{bands[1].name}</li>
-        <li>{bands[2].name}</li>
-    </ul>
-    """)
+    return render(request, "listings/hello.html", context={"bands" : bands})
 
 def about(request):
-    return HttpResponse("<h1>About us</h1><p>We love merch !</p>")
+    return render(request, "listings/about.html")
 
 def listings(request):
     title = Title.objects.all()
-    return HttpResponse(f"""
-    <h1>Listings</h1>
-    <p>Annonces</p>
-    <ul>
-        <li>{title[0].name}</li>
-        <li>{title[1].name}</li>
-        <li>{title[2].name}</li>
-        <li>{title[3].name}</li>
-    </ul>""")
+    return render(request, "listings/listings.html", context={"title" : title})
 
 def contact(request):
-    return HttpResponse("<h1>Contact</h1><p>Contact us !</p>")
+    return render(request, "listings/contact.html")
