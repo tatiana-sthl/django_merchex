29c10e7f2f81010c728fc79310e1578c55bb0a7f
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-03-22 09:51:07 +0100

    add informations in models.py

--- listings/models.py
+++ listings/models.py
@@ -1,7 +1,30 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = "HH"
+        SYNTH_POP = "SP"
+        ALTERNATIVE_ROCK = "AR"
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Title(models.Model):
+    
+    class Type(models.TextChoices):
+        Records = "R"
+        Clothing = "C"
+        Posters = "P"
+        Miscellaneous = "M"
+
     name = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=True)
+    year = models.fields.IntegerField(null=True, blank=True)
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
