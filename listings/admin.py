979f3d2e1b22c423e8a21884c718e8a8f5c0916f
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-03-22 12:43:09 +0100

    add changes (it's very specific)

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from listings.models import Band, Title
 
-# Register your models here.
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ("name", "year_formed", "genre")
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Title)
--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -15,6 +16,9 @@ class Band(models.Model):
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
 class Title(models.Model):
     
     class Type(models.TextChoices):
@@ -25,6 +29,12 @@ class Title(models.Model):
 
     name = models.fields.CharField(max_length=100)
     description = models.fields.CharField(max_length=1000)
-    sold = models.fields.BooleanField(default=True)
+    sold = models.fields.BooleanField(default=False)
     year = models.fields.IntegerField(null=True, blank=True)
     type = models.fields.CharField(choices=Type.choices, max_length=5)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
